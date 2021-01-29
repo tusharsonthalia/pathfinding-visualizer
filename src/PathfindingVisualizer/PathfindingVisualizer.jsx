5aae5a0431d29d450f116db070e96f7d4b8c7520
tusharsonthalia <sonthaliatushar@gmail.com>
2021-01-29 18:45:15 +0530

    Added instructions on how to use

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,13 +1,13 @@
-import React, {Component, Fragment} from 'react';
-import Node from './Node/Node';
-import {dijkstra, getNodesInShortestPathOrder} from '../algorithms/dijkstra';
-import {Navbar, Legend} from './utils';
+import React, { Component, Fragment } from "react";
+import Node from "./Node/Node";
+import { dijkstra, getNodesInShortestPathOrder } from "../algorithms/dijkstra";
+import { Navbar, Legend, Instructions } from "./utils";
 
-import './PathfindingVisualizer.css';
+import "./PathfindingVisualizer.css";
 
-const START_NODE_ROW = 5;
+const START_NODE_ROW = 8;
 const START_NODE_COL = 15;
-const FINISH_NODE_ROW = 5;
+const FINISH_NODE_ROW = 8;
 const FINISH_NODE_COL = 35;
 
 export default class PathfindingVisualizer extends Component {
@@ -16,27 +16,28 @@ export default class PathfindingVisualizer extends Component {
     this.state = {
       grid: [],
       mouseIsPressed: false,
+      showInstructions: true,
     };
   }
 
   componentDidMount() {
     const grid = getInitialGrid();
-    this.setState({grid});
+    this.setState({ grid });
   }
 
   handleMouseDown(row, col) {
     const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
-    this.setState({grid: newGrid, mouseIsPressed: true});
+    this.setState({ grid: newGrid, mouseIsPressed: true });
   }
 
   handleMouseEnter(row, col) {
     if (!this.state.mouseIsPressed) return;
     const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
-    this.setState({grid: newGrid});
+    this.setState({ grid: newGrid });
   }
 
   handleMouseUp() {
-    this.setState({mouseIsPressed: false});
+    this.setState({ mouseIsPressed: false });
   }
 
   animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
@@ -50,7 +51,7 @@ export default class PathfindingVisualizer extends Component {
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
-          'node node-visited';
+          "node node-visited";
       }, 10 * i);
     }
   }
@@ -60,13 +61,13 @@ export default class PathfindingVisualizer extends Component {
       setTimeout(() => {
         const node = nodesInShortestPathOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
-          'node node-shortest-path';
+          "node node-shortest-path";
       }, 50 * i);
     }
   }
 
   visualizeDijkstra() {
-    const {grid} = this.state;
+    const { grid } = this.state;
     const startNode = grid[START_NODE_ROW][START_NODE_COL];
     const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
     const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
@@ -74,8 +75,22 @@ export default class PathfindingVisualizer extends Component {
     this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
   }
 
+  showInstructions() {
+    this.setState({ showInstructions: !this.state.showInstructions });
+  }
+
+  clearBoard() {
+    window.location.reload();
+  }
+
   render() {
-    const {grid, mouseIsPressed} = this.state;
+    const { grid, mouseIsPressed, showInstructions } = this.state;
+    var instructions;
+    if (showInstructions) {
+      instructions = <Instructions />;
+    } else {
+      instructions = null;
+    }
 
     return (
       <Fragment>
@@ -83,17 +98,31 @@ export default class PathfindingVisualizer extends Component {
         <Legend />
         <div className="button-grid">
           <button
+            className="btn btn-lg btn-outline-dark"
+            onClick={() => this.showInstructions()}
+          >
+            Instructions
+          </button>
+          <button
             className="btn btn-lg btn-outline-success"
-            onClick={() => this.visualizeDijkstra()}>
+            onClick={() => this.visualizeDijkstra()}
+          >
             Visualize Dijkstra's Algorithm
           </button>
+          <button
+            className="btn btn-lg btn-outline-danger"
+            onClick={() => this.clearBoard()}
+          >
+            Clear Board
+          </button>
         </div>
+        {instructions}
         <div className="grid">
           {grid.map((row, rowIdx) => {
             return (
               <div key={rowIdx}>
                 {row.map((node, nodeIdx) => {
-                  const {row, col, isFinish, isStart, isWall} = node;
+                  const { row, col, isFinish, isStart, isWall } = node;
                   return (
                     <Node
                       key={nodeIdx}
@@ -107,7 +136,8 @@ export default class PathfindingVisualizer extends Component {
                         this.handleMouseEnter(row, col)
                       }
                       onMouseUp={() => this.handleMouseUp()}
-                      row={row}></Node>
+                      row={row}
+                    ></Node>
                   );
                 })}
               </div>
@@ -121,7 +151,7 @@ export default class PathfindingVisualizer extends Component {
 
 const getInitialGrid = () => {
   const grid = [];
-  for (let row = 0; row < 11; row++) {
+  for (let row = 0; row < 15; row++) {
     const currentRow = [];
     for (let col = 0; col < 50; col++) {
       currentRow.push(createNode(col, row));
--- src/PathfindingVisualizer/utils.jsx
+++ src/PathfindingVisualizer/utils.jsx
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from "react";
 
 export class Navbar extends Component {
   render() {
@@ -29,7 +29,8 @@ export class Legend extends Component {
           <div>
             <div
               className="node"
-              style={{backgroundColor: 'rgba(0, 190, 218, 0.75)'}}></div>
+              style={{ backgroundColor: "rgba(0, 190, 218, 0.75)" }}
+            ></div>
             <div className="legend-desc">Visited Node</div>
           </div>
           <div>
@@ -39,9 +40,36 @@ export class Legend extends Component {
           <div>
             <div
               className="node"
-              style={{backgroundColor: 'rgb(255, 254, 106)'}}></div>
+              style={{ backgroundColor: "rgb(255, 254, 106)" }}
+            ></div>
             <div className="legend-desc">Shortest Path</div>
           </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+export class Instructions extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="container mt-4 text-secondary text-left instructions">
+          <p>
+            A Pathfinding Algorithm seeks to find the shortest path between two
+            points. This web application created in ReactJS visualizes several
+            pathfinding algorithms.
+          </p>
+          <p>
+            The algorithms used in this application is the{" "}
+            <strong>Dijkstra's Algorithm</strong> which is the father of
+            pathfinding algorithms; guarantees the shortest path. In order to visualize the shortest path between the two points,
+            click on the button above to visualize the Dijkstra's Algorithm.
+          </p>
+          <p>
+            Walls can be added by clicking on the grid. Walls are impenetrable,
+            meaning that a path cannot cross through them. Dragging through the
+            grid can also add walls.
+          </p>
         </div>
       </Fragment>
     );
